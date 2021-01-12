395ef9e1dbd44b02c9d4d8ccd98def6ae669474f
Stefano Franchini <stefano.franchini77@gmail.com>
2021-01-12 13:14:55 +0100

    vue socials

--- js/main.js
+++ js/main.js
@@ -2,7 +2,24 @@ let app = new Vue (
     {
         el: "#app",
         data: {
-
+            socials: [
+                {
+                    name: 'Facebook',
+                    iconClass: 'fab fa-facebook-f'
+                },
+                {
+                    name: 'Twitter',
+                    iconClass: 'fab fa-twitter'
+                },
+                {
+                    name: 'Instagram',
+                    iconClass: 'fab fa-instagram'
+                },
+                {
+                    name: 'YouTube',
+                    iconClass: 'fab fa-youtube'
+                },
+            ],
         },
         methods: {
 
